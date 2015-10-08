5fc4e4b1a0a91cb5a82680db62907431b4d9ff3d
James Curtis <james12cahs@gmail.com>
2015-10-08 16:49:19 -0700

    Current shoot blaster and destroy after a set interval of time scripts for the blaster pistol

---
+++ ShootBlaster.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShootBlaster : MonoBehaviour 
+{
+	//Variables Start-----------------
+
+	//This is terms for Launch position of blaster, and the blaster itself
+	public Rigidbody blasterPrefab;
+	public Transform ProjectileLaunch;
+
+	//This controls the blaster fire rate
+	public float nextFire=0;
+	public float fireRate=0.2f;
+
+	//Explosion Particles
+	public GameObject blasterExplosion;
+
+	//Variables End-------------------
+
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		//What happens when you fire the blaster
+		if (Input.GetButton ("Fire Weapon")&& Time.time > nextFire) 
+		{
+			//This allows the blaster to be fired as a rigid body, and adds force to manipulate speed
+			Rigidbody blasterInstance;
+			blasterInstance = Instantiate(blasterPrefab, ProjectileLaunch.position, ProjectileLaunch.rotation) as Rigidbody;
+			blasterInstance.AddForce(ProjectileLaunch.up * 500);
+
+			//This is the control for how fast it can shoot another round
+			nextFire = Time.time + fireRate;
+		}
+	}
+}
+
+
